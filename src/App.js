cbb5e630e20bb98286324b71411241aa3c1cc0a5
lizth93 <lizth93@hotmail.com>
2022-06-06 18:46:58 -0500

    created the components ExpenseItem and ExpenseDate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  const expenses = [
+    {
+      title: "expenses car",
+      amount: 297,
+      date: new Date(2022, 6, 6),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
