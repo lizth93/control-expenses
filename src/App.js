b7173229ab0647099e08cf7fb778ac8b9041f4f1
lizth93 <lizth93@hotmail.com>
2022-06-07 10:16:56 -0500

    Added module expenses

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
     {
-      title: "expenses car",
+      title: "testing car",
       amount: 297,
       date: new Date(2022, 6, 6),
+    },
+
+    {
+      title: "house",
+      amount: 300,
+      date: new Date(2022, 6, 10),
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
