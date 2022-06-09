f60dee195affd9c390dec72aa6b6538fa3ce9495
lizth93 <lizth93@hotmail.com>
2022-06-09 10:06:12 -0500

    Deleted dummy data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,8 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import { useState } from "react";
 
-const DUMMY_EXPENSES = [
-  {
-    id: 1,
-    title: "testing car",
-    amount: 297,
-    date: new Date(2022, 6, 6),
-  },
-
-  {
-    id: 2,
-    title: "house",
-    amount: 300,
-    date: new Date(2022, 6, 10),
-  },
-];
-
 function App() {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState([]);
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => [expense, ...prevExpenses]);
